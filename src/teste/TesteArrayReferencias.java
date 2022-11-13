f6f1ec1502e0efbdc987d27983b7ff2e5fe00b6a
Camila Marques de Lima <95050431+CamilaLima21@users.noreply.github.com>
2022-11-13 13:18:18 -0300

    Testes com Arrays

--- src/teste/TesteArrayReferencias.java
+++ src/teste/TesteArrayReferencias.java
@@ -1,26 +1,31 @@
 package teste;
 
+import modelo.Cliente;
 import modelo.ContaCorrente;
+import modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
 		//int[] idades = new int [5];
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] referencias = new Object[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(001, 22);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
-		ContaCorrente cc2 = new ContaCorrente(002, 33);
-		contas[1] = cc2;
+		ContaPoupanca cc2 = new ContaPoupanca(002, 33);
+		referencias[1] = cc2;
 		
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
+		
+		//Object referenciaGenerica = referencias[1];
+		
+		//System.out.println(referenciaGenerica[1].getNumero());
+		
+		ContaPoupanca ref = (ContaPoupanca) referencias[1];
 		System.out.println(cc2.getNumero());
-		
-		System.out.println(contas[1].getNumero());
-		
-		ContaCorrente ref = contas[1];
-		
 		System.out.println(ref.getNumero());
 		
 	}
